cffda0d664173a17f754f0c057267edc5f27bc32
Belar <belar@belardesign.com>
2017-03-22 12:01:33 +0100

    Change message formatting

--- modules/rocketLaunch.js
+++ modules/rocketLaunch.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const chalk = require('chalk');
 
 function getLaunchDetails(launchId, callback) {
   axios.get('https://launchlibrary.net/1.2/launch/' + launchId).then((response) => {
@@ -9,12 +10,21 @@ function getLaunchDetails(launchId, callback) {
 exports.nextLaunch = function (argv) {
   axios.get('https://launchlibrary.net/1.2/launch/next/1').then((response) => {
     let next = response.data.launches[0];
+
     if (argv.details) {
       getLaunchDetails(next.id, function (nextDetails) {
-        console.log(nextDetails.id, nextDetails.name, nextDetails.net, nextDetails.vidURLs, nextDetails.rocket.name);
+        let title = chalk.yellow('Next launch:') + ' ' + nextDetails.id + ' ' + nextDetails.name;
+        let schedule = chalk.cyan('Scheduled launch attempt:') + ' ' + nextDetails.net;
+        let broadcasts = chalk.cyan('Broadcasts:') + ' ' + nextDetails.vidURLs;
+        let rocket = chalk.cyan('Rocket:') + ' ' + nextDetails.rocket.name;
+
+        console.log(title + '\n' + schedule + '\n' + broadcasts + '\n' + rocket);
       });
     } else {
-      console.log(next.id, next.name, next.net);
+      let title = chalk.yellow('Next launch:') + ' ' + next.id + ' ' + next.name;
+      let schedule = chalk.cyan('Scheduled launch attempt:') + ' ' + next.net;
+
+      console.log(title + '\n' + schedule);
     }
   });
 }
