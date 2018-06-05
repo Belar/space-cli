7461ffe9dcf7c46d320087ea166b9cabe1ce8ba3
Belar <belar@belardesign.com>
2018-06-05 20:08:59 +0200

    Change d to v, add verbose alias

--- src/index.js
+++ src/index.js
@@ -13,16 +13,17 @@ const argv = yargs // eslint-disable-line
   .command('about', 'Info about the CLI', info.about)
   .command('next', 'Get next rocket launch',
     function (yargs) {
-      return yargs.option('d', {
-        alias: 'details',
-        describe: 'Details about the next launch'
-      }).option('tz', {
-        alias: 'timezone',
-        describe: 'Define time zone for time info e.g. America/New_York, Europe/Paris, Asia/Shanghai'
-      }).option('lt', {
-        alias: 'limit',
-        describe: 'Define amount of upcoming events to show'
-      });
+      return yargs
+        .option('v', {
+          alias: ['verbose', 'details'],
+          describe: 'Details about the next launch'
+        }).option('tz', {
+          alias: 'timezone',
+          describe: 'Define time zone for time info e.g. America/New_York, Europe/Paris, Asia/Shanghai'
+        }).option('lt', {
+          alias: 'limit',
+          describe: 'Define amount of upcoming events to show'
+        });
     },
     rocketLaunch.nextLaunch
   )
