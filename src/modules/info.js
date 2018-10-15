5e62bc3cebeb562aa409a1ad9f47496f8db7ab10
Belar <belar@belardesign.com>
2018-10-15 19:51:04 +0200

    Extract about message into variables

--- src/modules/info.js
+++ src/modules/info.js
@@ -2,5 +2,8 @@ const chalk = require('chalk');
 const helpers = require('../helpers');
 
 exports.about = function () {
-  helpers.printMessage(chalk.green('Welcome to Space CLI') + ' ' + '- a CLI for space information' + '\n\n' + 'Credits:' + '\n' + 'https://launchlibrary.net/ - API documentation for upcoming launches');
+  const welcome = chalk`{green Welcome to Space CLI} - a CLI for space information`;
+  const credits = `Credits:\nhttps://launchlibrary.net/ - API documentation for upcoming launches`;
+
+  helpers.printMessage(`${welcome}\n\n${credits}`);
 };
