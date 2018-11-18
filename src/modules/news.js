1731d7b5256fb213ccae6cfda0387716dc88f7a2
Belar <belar@belardesign.com>
2018-11-18 21:10:58 +0100

    Add fetching and listing articles

--- src/modules/news.js
+++ src/modules/news.js
@@ -1,3 +1,27 @@
+const axios = require('axios');
+const chalk = require('chalk');
+
+const helpers = require('../helpers');
+
 exports.listArticles = function (argv) {
-  console.log('Articles', argv);
+  const articleCount = argv.limit > 1 ? argv.limit : 1;
+
+  axios.get('https://api.spaceflightnewsapi.net/articles/', {
+    params: {
+      limit: articleCount
+    }
+  }).then((response) => {
+    const articlesCount = response.data.length;
+
+    for (let i = 0; i < articlesCount; i++) {
+      const article = response.data[i];
+
+      const title = chalk`${article.title} {yellow ${article.news_site_long}}`;
+
+      helpers.printMessage(`${title}`);
+    }
+  }).catch(error => {
+    const errorMessage = `${error.code}: ${error.message}`;
+    helpers.printError(errorMessage);
+  });
 };
